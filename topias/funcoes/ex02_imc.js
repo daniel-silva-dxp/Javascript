1823b757cfbb9f008f39c24f6de2aa79c6599862
Daniel Silva <dani3l.dxp@gmail.com>
2020-12-15 12:31:13 -0300

    feat: refatorando exercicio IMC

--- topias/funcoes/ex02_imc.js
+++ topias/funcoes/ex02_imc.js
@@ -6,7 +6,7 @@ const isNumber = (val) => {
   return typeof val === "number" ? true : false;
 };
 
-const toCm = (height) => height / 100;
+const toMt = (height) => height / 100;
 
 const imc = (weight, height) => {
   if (!getLength(weight, height)) throw new Error("Insert weight and height");
@@ -14,7 +14,7 @@ const imc = (weight, height) => {
   if (!isNumber(weight) || !isNumber(height))
     throw new Error("Insert a number");
 
-  let imc = weight / (toCm(height) * toCm(height));
+  let imc = weight / (toMt(height) * toMt(height));
   return `IMC: ${imc.toFixed(2)}`;
 };
 
