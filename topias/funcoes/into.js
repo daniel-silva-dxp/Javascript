80249798de7e73cfc9ecb2ace271374222cb2b27
Daniel Silva <dani3l.dxp@gmail.com>
2020-12-15 09:04:32 -0300

    feat: callback e closure

--- topias/funcoes/into.js
+++ topias/funcoes/into.js
@@ -11,12 +11,37 @@ function teste() {
 })("IIFE");
 
 // Arguments
-const sum = function () {
+const sum = function (arr) {
   let total = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    total += arguments[i];
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
   }
-  return total;
+  let media = total / arr.length;
+  return media.toFixed(2);
 };
 
-console.log(sum(10, 15, 7, 22));
+console.log(sum([10, 15, 7, 22]));
+
+// Callback
+function foo(callback) {
+  let arr = [];
+  let i = 0;
+  while (i <= 10) {
+    arr.push(parseInt(Math.random() * 10));
+    i++;
+  }
+  typeof callback === "function"
+    ? console.log(callback(arr))
+    : console.log("Not a Function");
+}
+
+foo(sum);
+
+function bar(x) {
+  return function (y) {
+    return x * y;
+  };
+}
+
+const n = bar(10);
+console.log(n(100));
