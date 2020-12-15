0606dd1418e1993ff8b2da5473b7bb9c62988abc
Daniel Silva <dani3l.dxp@gmail.com>
2020-12-15 08:22:07 -0300

    feat: arguments

--- topias/funcoes/into.js
+++ topias/funcoes/into.js
@@ -9,3 +9,14 @@ function teste() {
 (function (msg) {
   console.log(msg);
 })("IIFE");
+
+// Arguments
+const sum = function () {
+  let total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  return total;
+};
+
+console.log(sum(10, 15, 7, 22));
